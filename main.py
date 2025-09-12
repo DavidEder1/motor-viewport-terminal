f6ae58119791b0641268149ebca6d12a1909f068
David Eder <david.eder.m.s@gmail.com>
2025-09-12 00:52:40 -0300

    mod: corrige formatação de strings e adiciona comentários em safeAddstr e safeAddch

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import curses
 
 def mover(x, y, key, xmax, ymax):
     movimento = {
-        ord(w): (0, -1), curses.KEY_UP: (0, -1),
-        ord(s): (0, 1), curses.KEY_DOWN: (0, 1),
-        ord(a): (-1, 0), curses.KEY_LEFT: (-1, 0),
-        ord(d): (1, 0), curses.KEY_RIGHT: (1, 0)
+        ord('w'): (0, -1),  curses.KEY_UP: (0, -1),
+        ord('s'): (0, 1),   curses.KEY_DOWN: (0, 1),
+        ord('a'): (-1, 0),  curses.KEY_LEFT: (-1, 0),
+        ord('d'): (1, 0),   curses.KEY_RIGHT: (1, 0)
     }
 
     if key in movimento:
@@ -28,12 +28,12 @@ def safeAddstr(stdscr, y, x, string):
             return
         stdscr.addstr(y, x, string[:maxLen])
     except curses.error:
-        pass
+        pass # Adicionar erros em um log
 
 def safeAddch(stdscr, y, x, ch):
     try:
         ymax, xmax = stdscr.getmaxyx()
         if 0 <= y < ymax and 0 <= x < xmax:
-            stdscr.addch(y, x, ch)
+            stdscr.addch(y, x, ch) # Adicionar cores
     except curses.error:
         pass
